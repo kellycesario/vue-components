a53077a36bdeb9e7c4e11184396aedb7bf62d8f3
kellycesario <kellycesario@gmail.com>
2023-03-13 14:01:43 -0300

    Fix: change createWebHistory to createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 import Home from "@views/ViewHome.vue";
 import AboutUs from "@views/ViewAboutUs.vue";
@@ -42,7 +42,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
