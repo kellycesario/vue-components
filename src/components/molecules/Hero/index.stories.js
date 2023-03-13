9582c5945263a15cea5f17a8fe0fc2ec9f44bbb3
kellycesario <kellycesario@gmail.com>
2023-03-13 14:01:54 -0300

    Add: insert args

--- src/components/molecules/Hero/index.stories.js
+++ src/components/molecules/Hero/index.stories.js
@@ -1,8 +1,15 @@
 import Hero from "./index.vue";
+import banner from "/images/hero-banner.png";
 
 export default {
   title: "Molecules/Hero",
   component: Hero,
+  args: {
+    title: "Find your perfect place to stay",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    image: banner,
+    video: "https://www.youtube.com/watch?v=09839DpTctU&ab_channel=Eagles",
+  },
   argTypes: {
     video: {
       table: {
